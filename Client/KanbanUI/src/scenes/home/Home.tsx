a5aa1cda8a84bdfb0f739774e2e8a70b3858fd51
abir-hub <abirbasumatary11@gmail.com>
2025-02-09 16:46:43 +0530

    calender added

--- Client/KanbanUI/src/scenes/home/Home.tsx
+++ Client/KanbanUI/src/scenes/home/Home.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./Home.scss";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [value, onChange] = useState<Value>(new Date());
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Calendar onChange={onChange} value={value} />
+    </>
+  );
 };
 export default Home;
